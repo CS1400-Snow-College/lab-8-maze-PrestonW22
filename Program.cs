594a91aa05e43cd3d9f8c7e4accd42580a2265d2
Preston <prestondeanwaters@gmail.com>
2025-11-01 11:35:34 -0600

    step 1

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿//preston waters, maze, 10/25/31
+Console.Title = "Maze Game";
+
+        // Step 1: Display intro
+        Console.WriteLine("Welcome to the Maze Game!");
+        Console.WriteLine("Use the arrow keys to move. Find the '*' to win.");
+        Console.WriteLine("Press any key to start...");
+        Console.ReadKey(true);
+
+        
